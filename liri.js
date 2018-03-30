3df04914430699d2a45d34687a14c87ce1ad9158
Kris <kris.bierma@gmail.com>
2018-03-29 20:33:49 -0500

    adding files

---
+++ liri.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+
+var twit = require("./keys.js").twitter;
+var spot = require("./keys.js").spotify;
+
+// var spotify = new Spotify(keys.spotify);
+// var client = new Twitter(keys.twitter);
