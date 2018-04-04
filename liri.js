f7e31c5b2224974ffa3611fe2ed92374dae7991c
Kris <kris.bierma@gmail.com>
2018-04-04 10:30:08 -0500

    finished. added readMe, ability to write to log.txt

--- liri.js
+++ liri.js
@@ -1,15 +1,18 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
-
 var Spotify = require('node-spotify-api');
 var Twitter = require('twitter');
 var omdbApi = require("omdb-client");
+var fs = require("fs");
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
 var userCommand = process.argv[2];
+var song;
+var movie;
+
 
 if (userCommand === "my-tweets"){
     var params = {
@@ -19,15 +22,18 @@ if (userCommand === "my-tweets"){
     };
     client.get("statuses/user_timeline", params, function(error, tweets, response){
         if (!error){
+            logIt("my-tweets: ");
             for (var i=0; i<tweets.length; i++){
                 console.log(tweets[i].text);
+                var dataToApp = tweets[i].text;
+                logIt("\n"+dataToApp);
             }
+            console.log("Data logged to log.txt.");
         }
     });
-};
+}
 
-var song;
-if (userCommand === "spotify-this-song"){
+else if (userCommand === "spotify-this-song"){
     if (!process.argv[3]){
         song = "ace of base";  
         gotSong(song);
@@ -36,7 +42,33 @@ if (userCommand === "spotify-this-song"){
         song = process.argv[3];
         gotSong(song);
     }
-};
+}
+
+else if (userCommand === "movie-this"){
+    if (!process.argv[3]){
+        movie = "Mr. Nobody";  
+        gotMovie(movie);
+    }
+    else {
+        movie = process.argv[3];
+        gotMovie(movie);
+    }
+}
+
+else if (userCommand === "do-what-it-says"){
+    fs.readFile("random.txt", "utf8", function(error, data){
+        if (error) return error;
+        var randomArr = data.split(",");
+        song = randomArr[1];
+        userCommand = randomArr[0];
+        logIt("do-what-it-says");
+        gotSong(song);
+    })
+}
+
+else {
+    console.log("Please type in a command: \n my-tweets \n spotify-this-song 'song name here' \n movie-this 'song name here' \n do-what-it-says");
+}
 
 function gotSong(song){
     spotify.search({
@@ -49,26 +81,16 @@ function gotSong(song){
         }
         else{
             var location = data.tracks.items[0];
-            // console.log(location);
-            
             console.log("Song title: "+location.name);
             console.log("Album: " +location.album.name);
             console.log("Artist: "+location.artists[0].name);
             console.log("Preview: "+location.external_urls.spotify);
-}})
+            var space = ", ";
+            dataToApp=("\n"+"spotify-this-song: "+location.name+space+location.album.name+space+location.artists[0].name+space+location.external_urls.spotify);
+            logIt(dataToApp);
+            console.log("Data logged to log.txt.");
+    }})
 };
-
-var movie;
-if (userCommand === "movie-this"){
-    if (!process.argv[3]){
-        movie = "Mr. Nobody";  
-        gotMovie(movie);
-    }
-    else {
-        movie = process.argv[3];
-        gotMovie(movie);
-    }
-}
 
 function gotMovie(movie){
     var params = {
@@ -77,7 +99,6 @@ function gotMovie(movie){
     }
 
     omdbApi.get(params,function(error, data){
-        // console.log(data);
         console.log("Title: "+data.Title);
         console.log("Year: "+data.Year);
         console.log("IMDB rating: "+data.Ratings[0].Value);
@@ -86,5 +107,14 @@ function gotMovie(movie){
         console.log("Language: "+data.Language);
         console.log("Plot: "+data.Plot);
         console.log("Actors: "+data.Actors);
+        var space = ", ";
+        dataToApp=("\n"+"movie-this: "+data.Title+ space+data.Year+space+data.Ratings[0].Value+space+data.Ratings[1].Value+space+data.Country+space+data.Language+space+data.Plot+data.Actors);
+        logIt(dataToApp);
+        console.log("Data logged to log.txt.");
     });
 };
+
+function logIt(dataToAppend){
+    fs.appendFile("log.txt", dataToAppend, function(err){if (err) return err;
+    });
+}
